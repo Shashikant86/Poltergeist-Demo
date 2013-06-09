75762a1463446cf0360d7d86896c15da0b98dc38
Shashikant86 <shashikant.jagtap@hotmail.co.uk>
2013-06-09 10:28:52 +0100

    Tidy up step definition

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -2,22 +2,21 @@ Given(/^I am on the home page$/) do
 puts  visit "http://www.google.com"
 end
 
-Then(/^I should see "(.*?)"$/) do |text|
-  page.should have_content text
-  
-  #page.driver.resize(20,30)
- # page.save_screenshot("/Users/jagtas01/Desktop/test.pdf")
-  #puts page.within_window
- # puts page.driver.network_traffic
-  #puts page.driver.cookies
-  #page.response_headers.to_a
-
-
-end
-
 When /^I fill in "([^"]*)" with "([^"]*)"$/ do |element, text|
  fill_in element, with: text
- page.driver.debug
+ #page.driver.debug
 end
+
+Then(/^I should see "(.*?)"$/) do |text|
+  page.should have_content text
+  #page.driver.resize(20,30)
+  # page.save_screenshot("/path/to/test.pdf")
+  #puts page.within_window
+  # puts page.driver.network_traffic
+  #puts page.driver.cookies
+  #page.response_headers.to_a
+end
+
+
 
 
